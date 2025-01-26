1f1fd31fb453b0d607b25e042e0569f818d3ba8f
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2025-01-26 14:28:04 +0100

    added projection.project

--- src/coherence/projection.clj
+++ src/coherence/projection.clj
@@ -36,3 +36,48 @@
   (-> m
       (assoc-many (:assoc patch))
       (dissoc-many (:dissoc patch))))
+
+;;; projection
+
+(defn- aggr-map->vec
+  [m]
+  (into []
+        (mapcat (fn [[kind children]]
+                  (map (fn [[id aggr]]
+                         {:id [kind id]
+                          :aggregate aggr})
+                       children)))
+        m))
+
+(defn project
+  "Applies patches from an event stream to affected aggregates from `start`
+   onwards. The initial state of an aggregate is either created with `loadf` or
+   taken from the very first replayed event. Returns a transducer if `coll` is
+   not provided."
+  ([start loadf]
+   (fn [rf]
+     (let [m (volatile! {})
+           replayed (volatile! false)]
+       (fn
+         ([] (rf))
+         ([result]
+          (rf result))
+         ([result {seq-no :seq-no {[kind id] :aggregate patch :patch} :action}]
+          (if (< seq-no start)
+            (do
+              (vswap! m update-in [kind id] apply-patch patch)
+              result)
+            (let [patched (-> (or (get-in @m [kind id])
+                                  (loadf kind id))
+                              (apply-patch patch))]
+              (vswap! m assoc-in [kind id] patched)
+              (let [value (if @replayed
+                            {:seq-no seq-no
+                             :aggregates [{:id [kind id]
+                                           :aggregate patched}]}
+                            {:seq-no seq-no
+                             :aggregates (aggr-map->vec @m)})]
+                (vreset! replayed true)
+                (rf result value)))))))))
+  ([start loadf coll]
+   (sequence (project start loadf) coll)))
--- test/coherence/projection_tests.clj
+++ test/coherence/projection_tests.clj
@@ -1,6 +1,9 @@
 (ns coherence.projection-tests
   (:require [clojure.test :refer [deftest testing is]]
-            [coherence.projection :refer [apply-patch]]))
+            [coherence.projection :refer [apply-patch project]]
+            [coherence.test-utils :as utils]
+            [spy.assert :as assert]
+            [spy.core :as spy]))
 
 (deftest test-apply-patch
   (testing "assoc key"
@@ -56,3 +59,109 @@
                  :dissoc [:c]}
           result (apply-patch  {:a {:d 2} :c 3 :d 4} patch)]
       (is (= {:a {:b 1 :d 2} :d 4} result)))))
+
+(deftest test-project
+  (let [events [{:seq-no 1
+                 :source ::test
+                 :timestamp (utils/now)
+                 :action {:reason ::create
+                          :actor [:test 1]
+                          :aggregate [:thing 1]
+                          :patch {:assoc {:a 1}}}
+                 :triggers #{[:trg 1]}}
+                {:seq-no 2
+                 :source ::test
+                 :timestamp (utils/now)
+                 :action {:reason ::create
+                          :actor [:test 1]
+                          :aggregate [:thing 2]
+                          :patch {:assoc {:a 1}}}
+                 :triggers #{[:trg 2]}}
+                {:seq-no 3
+                 :source ::test
+                 :timestamp (utils/now)
+                 :action {:reason ::update
+                          :actor [:test 1]
+                          :aggregate [:thing 1]
+                          :patch {:assoc {:a 2}}}
+                 :triggers #{[:trg 1]}}
+                {:seq-no 5
+                 :source ::test
+                 :timestamp (utils/now)
+                 :action {:reason ::update
+                          :actor [:test 1]
+                          :aggregate [:thing 2]
+                          :patch {:assoc {:a 2}}}
+                 :triggers #{[:trg 2]}}
+                {:seq-no 6
+                 :source ::test
+                 :timestamp (utils/now)
+                 :action {:reason ::update
+                          :actor [:test 1]
+                          :aggregate [:thing 1]
+                          :patch {:assoc {:a 3}}}
+                 :triggers #{[:trg 1]}}]]
+    (testing "no replay"
+      (letfn [(loadf [_ id]
+                (case id
+                  1 {:a 2}
+                  2 {:a 1}))]
+        (testing "transducer"
+          (let [loadf' (spy/spy loadf)
+                result (into []
+                             (comp (filter #(>= (:seq-no %) 5))
+                                   (project 5 loadf'))
+                             events)]
+            (is (= 2 (count result)))
+            (is (= {:seq-no 5
+                    :aggregates [{:id [:thing 2], :aggregate {:a 2}}]}
+                   (result 0))
+                (is (= {:seq-no 6
+                        :aggregates [{:id [:thing 1], :aggregate {:a 3}}]}
+                       (result 1))))
+            (assert/called-n-times? loadf' 2)
+            (assert/called-with? loadf' :thing 1)
+            (assert/called-with? loadf' :thing 2)))
+
+        (testing "sequence"
+          (let [loadf' (spy/spy loadf)
+                result (vec (project 5
+                                     loadf'
+                                     (filter #(>= (:seq-no %) 5) events)))]
+            (is (= 2 (count result)))
+            (is (= {:seq-no 5
+                    :aggregates [{:id [:thing 2], :aggregate {:a 2}}]}
+                   (result 0))
+                (is (= {:seq-no 6
+                        :aggregates [{:id [:thing 1], :aggregate {:a 3}}]}
+                       (result 1))))
+            (assert/called-n-times? loadf' 2)
+            (assert/called-with? loadf' :thing 1)
+            (assert/called-with? loadf' :thing 2)))))
+
+    (testing "replay"
+      (testing "transducer"
+        (let [loadf (spy/spy (constantly {}))
+              result (into [] (project 5 loadf) events)]
+          (is (= 2 (count result)))
+          (is (= {:seq-no 5
+                  :aggregates [{:id [:thing 1], :aggregate {:a 2}}
+                               {:id [:thing 2], :aggregate {:a 2}}]}
+                 (result 0))
+              (is (= {:seq-no 6
+                      :aggregates [{:id [:thing 1], :aggregate {:a 3}}]}
+                     (result 1))))
+          (assert/not-called? loadf)))
+
+      (testing "sequence"
+        (let [loadf (spy/spy (constantly {}))
+              result (vec (project 5 loadf events))]
+          (is (= 2 (count result)))
+          (is (= {:seq-no 5
+                  :aggregates [{:id [:thing 1], :aggregate {:a 2}}
+                               {:id [:thing 2], :aggregate {:a 2}}]}
+                 (result 0))
+              (is (= {:seq-no 6
+                      :aggregates [{:id [:thing 1], :aggregate {:a 3}}]}
+                     (result 1))))
+          (assert/not-called? loadf))))))
